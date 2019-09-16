43482ecaddac2600b45896939ae4f64378b01b5a
James Tisdale <james.tisdale89@gmail.com>
2019-09-16 13:43:14 -0500

    finished

---
+++ ExampleOfClasses/ExampleOfClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExampleOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nintendoSwitch = new Product();
+            nintendoSwitch.Price = 299.99;
+            nintendoSwitch.Name = "Nintendo Switch";
+            Console.WriteLine(nintendoSwitch.Name + " " + nintendoSwitch.Price);
+        }
+    }
+}
